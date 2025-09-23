36ae0df90459c78072f158e05f6229db4cca098f
Bogdan Bogdanov <greenwookez@gmail.com>
2025-09-23 18:26:30 +0300

    ci: fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,52 +18,6 @@ const eslintConfig = [
     'prettier', // always keep prettier LAST
   ),
   {
-    plugins: ['import'],
-    rules: {
-      '@typescript-eslint/ban-ts-comment': 'warn',
-      '@typescript-eslint/no-empty-object-type': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          args: 'after-used',
-          ignoreRestSiblings: false,
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^(_|ignore)',
-        },
-      ],
-      'import/order': [
-        'error',
-        {
-          groups: [
-            'builtin', // Node.js built-ins
-            'external', // npm packages
-            'internal', // aliases like @app/**
-            ['parent', 'sibling', 'index'], // relative
-            'object',
-            'type',
-          ],
-          pathGroups: [
-            { pattern: 'react', group: 'external', position: 'before' },
-            { pattern: 'next/**', group: 'external', position: 'after' },
-            { pattern: '@app/**', group: 'internal', position: 'before' },
-          ],
-          pathGroupsExcludedImportTypes: ['react', 'builtin'],
-          'newlines-between': 'always',
-          alphabetize: { order: 'asc', caseInsensitive: true },
-        },
-      ],
-    },
-    settings: {
-      'import/resolver': {
-        typescript: {},
-      },
-    },
-  },
-  {
     ignores: ['.next/'],
   },
 ]
